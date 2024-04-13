b37f40c9882c861622f253de1a34366843343c5d
ElaineQT <wangqianteng@gmail.com>
2024-04-13 23:49:53 +0800

    Add custom sentiment classification

--- indexing/data/data_preprocessing.py
+++ indexing/data/data_preprocessing.py
@@ -24,49 +24,29 @@ def edit_post_date(json_file):
     with open(json_file, 'w') as file:
         json.dump(data, file, indent=4)
 
-def convert_json_to_csv(json_file, csv_file):
-    with open(json_file, 'r') as file:
-        data = json.load(file)
-
-        fieldnames = data[0].keys()
-
-    with open(csv_file, 'w', newline='', encoding='utf-8') as file:
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(data)
-
-def process_data(comment_file, custom_cluster, custom_sentiments, result_file):
-    comment_df = pd.read_csv(comment_file)
-    # custom_cluster_df = pd.read_csv(custom_cluster)
-    custom_sentiments_df = pd.read_csv(custom_sentiments)
+def process_data(csv_file, custom_cluster, result_file):
+    comment_df = pd.read_csv(csv_file)
+    custom_cluster_df = pd.read_csv(custom_cluster)
 
     comment_df.drop_duplicates(inplace=True)
     comment_df = comment_df[comment_df['post_comment'] != '[removed]']
     comment_df = comment_df[comment_df['post_comment'] != '[deleted]']
 
-    comment_df.drop("sentiment", axis=1, inplace=True)
-    # comment_df = pd.merge(comment_df, custom_cluster_df[['post_comment', 'Cluster', 'subreddit']], on=['post_comment', 'subreddit'], how='right')
-    comment_df = pd.merge(comment_df, custom_sentiments_df[['post_comment', 'sentiment', 'subreddit']], on=['post_comment', 'subreddit'], how='right')
-    comment_df.rename(columns={"Cluster": "intent"}, inplace=True)
+    comment_df.drop(['senticnet_subjectivity', 'emotion', 'Unnamed: 0'], axis=1, inplace=True)
+    comment_df = pd.merge(comment_df, custom_cluster_df[['post_comment', 'subreddit', 'intent']], on=['post_comment', 'subreddit'], how='left')
     comment_df.drop_duplicates(inplace=True)
 
     comment_df.to_csv(result_file, index=False)
 
-def process_data_test():
-    cluster_df = pd.read_csv('data/custom_cluster.csv')
-    cluster_df.drop_duplicates(inplace=True)
-    cluster_df.to_csv('data/custom_cluster_test.csv', index=False)
-
 
 if __name__ == "__main__":
     json_file = 'data/dataset.json'
-    csv_file = 'data/classified_comments-cluster.csv'
-    csv_result_file = 'data/classified_comments-cluster.csv'
-    # custom_cluster = 'data/classified_comments-cluster.csv'
-    # custom_sentiments = 'data/custom_sentiments.csv'
+    csv_file = 'data/classified_comments-sentiment.csv'
+    custom_cluster = 'data/classified_comments-cluster.csv'
+    result_file = 'data/classified_comments_final.csv'
 
-    # process_data(csv_file, custom_cluster, custom_sentiments, csv_result_file)
-    convert_csv_to_json(csv_result_file, json_file)
-    # process_data_test()
+    # process_data(csv_file, custom_cluster, result_file)
+    # convert_csv_to_json(result_file, json_file)
+    edit_post_date(json_file)
     
     print("Done")
